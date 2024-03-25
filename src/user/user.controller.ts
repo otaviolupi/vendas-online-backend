ac1ef8a7a74cf6e9442e991b912be4be86bd16e7
Otávio Lupi <otavio.lupi2@gmail.com>
2024-03-25 17:23:43 -0300

    feat: create user (POST)

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+    
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto) {
+        return {
+            ...createUser,
+            password: undefined
+        }
+    }
+    
+}
